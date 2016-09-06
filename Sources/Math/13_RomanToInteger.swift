34611db10ac0c0112f8c6fef3d30188686f52efd
yansong li <857367901@qq.com>
2016-09-05 22:24:52 -0400

    Add 263 Ugly Number

---
+++ Sources/Math/13_RomanToInteger.swift
@@ -0,0 +1,9 @@
+//
+//  13_RomanToInteger.swift
+//  LeetcodeSwift
+//
+//  Created by yansong li on 2016-09-05.
+//  Copyright © 2016 YANSONG LI. All rights reserved.
+//
+
+import Foundation
---
+++ Sources/Math/263_UglyNumber.swift
@@ -0,0 +1,37 @@
+//
+//  263_UglyNumber.swift
+//  LeetcodeSwift
+//
+//  Created by yansong li on 2016-09-05.
+//  Copyright © 2016 YANSONG LI. All rights reserved.
+//
+
+import Foundation
+
+/**
+	Title:263 Ugly Number
+	URL: https://leetcode.com/problems/ugly-number/
+	Space: O(1)
+	Time: O(1)
+ */
+
+class UglyNumber_Solution {
+  func isUgly(num: Int) -> Bool {
+    guard num > 0 else {
+      return false
+    }
+    var loopNum = num
+    while loopNum != 1 {
+      if loopNum % 2 == 0 {
+        loopNum /= 2
+      } else if loopNum % 3 == 0 {
+        loopNum /= 3
+      } else if loopNum % 5 == 0 {
+        loopNum /= 5
+      } else {
+        return false
+      }
+    }
+    return true
+  }
+}
