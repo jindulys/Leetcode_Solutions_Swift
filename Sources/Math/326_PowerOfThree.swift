40c42ad8f5699a4d7e73a267eb5fc2ccf240fe4a
yansong li <857367901@qq.com>
2016-09-05 21:08:37 -0400

    Add 326 Power of Three

---
+++ Sources/Math/326_PowerOfThree.swift
@@ -0,0 +1,26 @@
+//
+//  326_PowerOfThree.swift
+//  LeetcodeSwift
+//
+//  Created by yansong li on 2016-09-05.
+//  Copyright © 2016 YANSONG LI. All rights reserved.
+//
+
+import Foundation
+
+/**
+	Title:326 Power of Three
+	URL: https://leetcode.com/problems/power-of-three/
+	Space: O(1)
+	Time: O(1)
+ */
+
+class PowerOfThree_Solution {
+  
+  func isPowerOfThree(n: Int) -> Bool {
+    guard n > 0 else {
+      return false
+    }
+    return 3486784401 % n == 0
+  }
+}
