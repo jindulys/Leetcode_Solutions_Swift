8aad87ac8f2bd9e53e917d500e1463819aac3ebf
yansong li <857367901@qq.com>
2016-09-14 08:45:35 -0400

    Add 63 Unique Paths II

---
+++ Sources/DynamicProgramming/279_PerfectSquares.swift
@@ -0,0 +1,37 @@
+//
+//  279_PerfectSquares.swift
+//  LeetcodeSwift
+//
+//  Created by yansong li on 2016-09-14.
+//  Copyright © 2016 YANSONG LI. All rights reserved.
+//
+
+import Foundation
+
+/**
+	Title:63 Unique Paths II
+	URL: https://leetcode.com/problems/perfect-squares/
+	Space: O(n)
+	Time: O(n^2)
+ */
+
+class PerfectSquares_Solution {
+  func numSquares(n: Int) -> Int {
+    guard n > 0 else {
+      return 0
+    }
+    // NOTE: perfectNums is an Array, which stores the minimum nums of square numbers
+    // to sum up for index 'i'.
+    var perfectNums = Array(count: n + 1, repeatedValue: Int.max)
+    perfectNums[0] = 0
+    for i in 1...n {
+      for j in 1...i {
+        if j * j > i {
+          break
+        }
+        perfectNums[i] = min(perfectNums[i], perfectNums[i - j * j] + 1)
+      }
+    }
+    return perfectNums[n]
+  }
+}
