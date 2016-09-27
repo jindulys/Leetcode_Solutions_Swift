178f9492186b0c4e914930481f25324931119427
yansong li <857367901@qq.com>
2016-09-27 00:06:47 -0400

    Add 72 Edit Distance

--- LeetcodeSwift/ViewController.swift
+++ LeetcodeSwift/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+		let test = EditDistance_Solution()
+		test.test()
   }
 
   override func didReceiveMemoryWarning() {
--- Sources/DynamicProgramming/72_EditDistance.swift
+++ Sources/DynamicProgramming/72_EditDistance.swift
@@ -8,14 +8,21 @@
 
 import Foundation
 
+/**
+	Title:72 Edit Distance
+	URL: https://leetcode.com/problems/edit-distance/
+	Space: O(NM)
+	Time: O(NM)
+*/
+
 class EditDistance_Solution {
   func minDistance(_ word1: String, _ word2: String) -> Int {
-    let word1Characters = Array(word1.characters)
-		let word2Characters = Array(word2.characters)
+    let word1Characters = [Character](word1.characters)
+		let word2Characters = [Character](word2.characters)
 		let word1Len = word1Characters.count
 		let word2Len = word2Characters.count
 		
-		var dp = Array(repeating: Array(repeating: 0, count: word2Len), count: word1Len)
+		var dp = Array(repeating: Array(repeating: 0, count: word2Len + 1), count: word1Len + 1)
 		
 		for i in 0...word1Len {
 			for j in 0...word2Len {
@@ -32,4 +39,9 @@ class EditDistance_Solution {
 		}
 		return dp[word1Len][word2Len]
   }
+	
+	func test() {
+		let test1 = minDistance("", "")
+		print(test1)
+	}
 }
