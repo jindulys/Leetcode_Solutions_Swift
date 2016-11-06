2bb6781022364230aced69b9555f41cb9fb28fac
yansong li <857367901@qq.com>
2016-11-05 20:36:29 -0400

    Add 206 Reverse LinkedList

---
+++ Sources/LinkedList/206_ReverseLinkedList.swift
@@ -0,0 +1,30 @@
+//
+//  206_ReverseLinkedList.swift
+//  LeetcodeSwift
+//
+//  Created by yansong li on 2016-11-05.
+//  Copyright © 2016 YANSONG LI. All rights reserved.
+//
+
+import Foundation
+
+/**
+	Title:206 Reverse LinkedList
+	URL: https://leetcode.com/problems/reverse-linked-list/
+	Space: O(1)
+	Time: O(N)
+ */
+
+class ReverseLinkedList_Solution {
+  func reverseList(_ head: ListNode?) -> ListNode? {
+    var temp: ListNode?
+    var first = head
+    while let validNode = first {
+      let second = validNode.next
+      validNode.next = temp
+      temp = validNode
+      first = second
+    }
+    return temp
+  }
+}
