b924f64f77357d6835184bf4c75b034295a80313
yansong li <857367901@qq.com>
2016-11-09 01:26:39 -0500

    Add in fast way for 145

--- Sources/145_BinaryTreePostorderTraversal.swift
+++ Sources/145_BinaryTreePostorderTraversal.swift
@@ -12,25 +12,49 @@ import Foundation
 	Title:145 Binary Tree Post Order Traversal
 	URL: https://leetcode.com/problems/binary-tree-postorder-traversal/
 	Space: O(N)
-	Time: O(N^2)
+	Time: O(N)
  */
 
 class BinaryTreePostOrderTraversal_Solution {
   func postorderTraversal(_ root: TreeNode?) -> [Int] {
     var stack: [TreeNode] = []
-    var result: [Int] = []
+    var resultList: ListNode? = ListNode(0)
+    let dummyFirst = resultList
     guard let validRoot = root else {
-      return result
+      return []
     }
     stack.push(validRoot)
     while let checkingNode = stack.pop() {
-      result.insert(checkingNode.val, at: 0)
+      resultList?.next = ListNode(checkingNode.val)
+      resultList = resultList?.next
       if let leftNode = checkingNode.left {
         stack.push(leftNode)
       }
       if let rightNode = checkingNode.right {
         stack.push(rightNode)
       }
+    }
+    let correctResult = self.reverseList(dummyFirst?.next)
+    return self.listNodeToArray(correctResult)
+  }
+  
+  func reverseList(_ head: ListNode?) -> ListNode? {
+    var temp: ListNode?
+    var first = head
+    while let validNode = first {
+      first = validNode.next
+      validNode.next = temp
+      temp = validNode
+    }
+    return temp
+  }
+  
+  func listNodeToArray(_ head: ListNode?) -> [Int] {
+    var result: [Int] = []
+    var realHead = head
+    while realHead != nil {
+      result.append(realHead!.val)
+      realHead = realHead?.next
     }
     return result
   }
